9ccade2b9014f3a837eae23d04eecfc3c1dff81b
Justin Tan <tanjustin217@gmail.com>
2024-11-13 10:33:20 -0700

    implemented dice and dice roll

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
+import Dice from "./components/Dice"
+import React from 'react'
 
 function App() {
+  const [dice, setDice] = React.useState(allNewDice())
 
+  function allNewDice() {
+    const randomNums = []
+    for (let i = 0; i<10; i++) {
+      let num = Math.ceil(Math.random() * 6)
+      randomNums.push({value:num,isHeld:false})
+    }
+    return randomNums
+  }
+  
+  function handleRoll() {
+    setDice(allNewDice())
+  }
+
+  const diceNums = dice.map((die) => {
+    return ( <Dice value={die.value}/>)
+  })
   return (
     <main className="main">
-      <div className="main--container"></div>
+      <div className="main--container">
+        <div className="dice--container">
+          {diceNums}
+        </div>
+        <button className="roll" onClick={handleRoll}>Roll</button>
+      </div>
     </main>
   )
 }
